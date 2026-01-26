ef4bda101eb941e66bf10e7b84ec44d74080eceb
vinayakdeshpande06 <vinayakdeshpande06@gmail.com>
2026-01-26 11:19:27 +0530

    Unused resources and memory error logs removed

--- src/main/java/com/cdac/hostel/repository/UserCacheRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cdac.hostel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cdac.hostel.model.UserCache;
-
-public interface UserCacheRepository
-        extends JpaRepository<UserCache, Long> {
-}
--- src/main/java/com/cdac/hostel/service/UserCacheService.java
+++
@@ -1,5 +0,0 @@
-package com.cdac.hostel.service;
-
-public class UserCacheService {
-
-}
